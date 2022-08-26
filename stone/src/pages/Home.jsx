037a54a14f30f34c3b98f8a4ee039ab492c19e35
sbocognano <sbocognano@squad.fr>
2022-08-26 11:39:33 +0200

    [Stone_Ecommerce][FRONT] Add Home/NavBar/AnnouncementBanner Pages & Components

---
+++ stone/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import AnnouncementBanner from '../components/AnnouncementBanner'
+
+const Home = () => {
+  return (
+    <div>
+      <AnnouncementBanner />
+      <Navbar />
+      
+    </div>
+  )
+}
+
+export default Home
